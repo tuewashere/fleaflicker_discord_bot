08eefcc5776def7ad0efbac97aa33000fe03d8f6
Tue Vu <ContactTueVu@gmail.com>
2022-09-19 10:34:50 -0700

    Update fleaflicker-discord-bot.py

--- fleaflicker-discord-bot.py
+++ fleaflicker-discord-bot.py
@@ -11,25 +11,15 @@ team_ids= {#LEAGUE_ID1: 'NAME OF OWNER', LEAGUE_ID2: 'NAME OF OWNER', ... }
 current = int(time.time() * 1000)
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
-client = discord.Client(intents = intents)
 response = (requests.get('https://www.fleaflicker.com/api/FetchLeagueActivity', params=league_id)).json()['items']
 data = json.dumps(response, sort_keys = True, indent = 4)
 trade_ids = []
-transactions_message = []
+activity_wire = []
 roster_message = []
 
-def team_owners():
-	for x in range (10):
-		ids = (requests.get('https://www.fleaflicker.com/api/FetchLeagueRosters', params=league_id)).json()['rosters'][x]['team']['id']
-		id_data = json.dumps(ids, sort_keys = True, indent = 4)
-		names = (requests.get('https://www.fleaflicker.com/api/FetchLeagueRosters', params=league_id)).json()['rosters'][x]['team']['name']
-		names_data = json.dumps(names, sort_keys = True, indent = 4)
-		if id_data in team_ids.keys():
-			roster_message.append(names_data + ' is ' + team_ids[id_data])
-	
 def transactions(obj, n):
 	obj = json.loads(obj)
-	if current - int(obj[n]['timeEpochMilli']) < 86400000: #remove a 0 to make it 10days to 1 day
+	if current - int(obj[n]['timeEpochMilli']) < 86400000: 
 		transaction = obj[n].get('transaction')
 		ir_change = obj[n].get('reserveChange')
 		if transaction: 
@@ -42,13 +32,13 @@ def transactions(obj, n):
 				other = obj[n].get('tradeBlock')
 				trade_block(other) 
 			else:
-				transactions_message.append((other['description'])) # commish change
+				activity_wire.append((other['description'])) 
 
 def add_remove_trade(obj):
 	trade = obj.get('tradeId')
 	if trade:
 		if trade not in trade_ids:
-			transactions_message.append('**TRADE**')
+			activity_wire.append('**TRADE**')
 			trade_details(obj)
 			trade_ids.append(trade)
 		else:
@@ -57,9 +47,9 @@ def add_remove_trade(obj):
 		info = obj.get('player')
 		owner_info = info.get('owner')
 		if owner_info == None:
-			transactions_message.append((info['proPlayer']['nameFull']) + ' was dropped')
+			activity_wire.append((info['proPlayer']['nameFull']) + ' was dropped')
 		else:
-			transactions_message.append(owner_info['name'] + ' added ' + info['proPlayer']['nameFull'])
+			activity_wire.append(owner_info['name'] + ' added ' + info['proPlayer']['nameFull'])
 
 def trade_details(obj):
 	draftPick = obj.get('draftPick')
@@ -67,26 +57,36 @@ def trade_details(obj):
 	if draftPick:
 		season = draftPick['season']
 		rnd =  draftPick['round']
-		transactions_message.append( obj['team']['name'] + ' receives ' + str(rnd)  + "th " + 'round pick in ' + str(season)) 
+		activity_wire.append( obj['team']['name'] + ' receives ' + str(rnd)  + "th " + 'round pick in ' + str(season)) 
 	elif player:
-		transactions_message.append( player['owner']['name'] + ' receives ' + player['proPlayer']['nameFull'])
+		activity_wire.append( player['owner']['name'] + ' receives ' + player['proPlayer']['nameFull'])
 	
 def trade_block(obj):
 	info = obj.get('player')
 	owner_info = info.get('owner')
 	if obj['removed']:
-		transactions_message.append(owner_info['name'] + ' removed ' + info['proPlayer']['nameFull'] + ' from the trade block')
+		activity_wire.append(owner_info['name'] + ' removed ' + info['proPlayer']['nameFull'] + ' from the trade block')
 	else:
-		transactions_message.append(owner_info['name'] + ' added ' + info['proPlayer']['nameFull'] + ' to the trade block')
+		activity_wire.append(owner_info['name'] + ' added ' + info['proPlayer']['nameFull'] + ' to the trade block')
 
 def reserve_change(obj):
 	info = obj.get('player')
 	owner_info = info.get('owner')
 	status = obj.get('removed')
 	if status:
-		transactions_message.append(owner_info['name'] + ' removed ' + info['proPlayer']['nameFull'] + ' from IR')
+		activity_wire.append(owner_info['name'] + ' removed ' + info['proPlayer']['nameFull'] + ' from IR')
 	else:
-		transactions_message.append(owner_info['name'] + ' added ' + info['proPlayer']['nameFull'] + ' to IR')
+		activity_wire.append(owner_info['name'] + ' added ' + info['proPlayer']['nameFull'] + ' to IR')
+
+
+def team_owners():
+	for x in range (10): # set to number of teams in your league
+		ids = (requests.get('https://www.fleaflicker.com/api/FetchLeagueRosters', params=league_id)).json()['rosters'][x]['team']['id']
+		id_data = json.dumps(ids, sort_keys = True, indent = 4)
+		names = (requests.get('https://www.fleaflicker.com/api/FetchLeagueRosters', params=league_id)).json()['rosters'][x]['team']['name']
+		names_data = json.dumps(names, sort_keys = True, indent = 4)
+		if id_data in team_ids.keys():
+			roster_message.append(names_data + ' is ' + team_ids[id_data])
 
 @bot.event
 async def on_ready():
@@ -96,10 +96,10 @@ async def on_ready():
 @tasks.loop(hours=24)
 async def msg1():
 	channel = bot.get_channel(channel_id)
-	if (len(transactions_message)) > 0:
+	if (len(activity_wire)) > 0:
 		await channel.send('**Transactions in the last 24 hours**')
-		await channel.send('\n'.join(transactions_message))
-		transactions_message.clear()
+		await channel.send('\n'.join(activity_wire))
+		activity_wire.clear()
 		trade_ids.clear()
 		print ('scheduled message sent')
 	else:
@@ -109,10 +109,10 @@ async def msg1():
 @msg1.before_loop
 async def before_msg1():
 	for x in range(60*60*24):
-		if dt.datetime.now().hour == 8:
+		if dt.datetime.now().hour == 17: # 24 hour clock set to when you want the message sent
+			print ('its time')
 			for x in range(30):
 				transactions(data, x)
-			print ('its time')
 			return
 		await asyncio.sleep(1)
 
@@ -123,7 +123,6 @@ async def SendMessage(msg):
 	roster_message.clear()
 
 bot.run(DISCORD_TOKEN)
-
 
 
 
